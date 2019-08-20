0f03520e08b4ebdd9544ca21f07fa3e92f8d1bdc
Seva Konyakhin <konyakhinvsevolod@mail.ru>
2019-08-20 17:17:21 +0300

    fix utils.py

--- data/utils.py
+++ data/utils.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-def _parse_function(filename, label, size):
+def _parse_function(filename, label, n_channels, size):
     """Obtain the image from the filename (for both training and validation).
     The following operations are applied:
         - Decode the image from jpeg format
@@ -8,7 +8,7 @@ def _parse_function(filename, label, size):
     """
     image_string = tf.read_file(filename)
 
-    image_decoded = tf.image.decode_jpeg(image_string, channels=1)
+    image_decoded = tf.image.decode_jpeg(image_string, channels=n_channels)
 
     # This will convert to float values in [0, 1]
     image = tf.image.convert_image_dtype(image_decoded, tf.float32)
@@ -20,5 +20,6 @@ def _parse_function(filename, label, size):
 
 def train_preprocess(filename, label, random_flip=True):
     """
+    Data Augmentation
     """
     
